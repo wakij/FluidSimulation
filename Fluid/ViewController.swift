e518cb2ad1187010fd3cab16a0832f3bb3379ff6
wakita <tomoshigesansai1600@gmail.com>
2025-02-08 09:56:03 +0900

    Initial Commit

---
+++ Fluid/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Fluid
+//
+//  Created by wakita tomoshige on 2025/02/08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ FluidTests/FluidTests.swift
@@ -0,0 +1,17 @@
+//
+//  FluidTests.swift
+//  FluidTests
+//
+//  Created by wakita tomoshige on 2025/02/08.
+//
+
+import Testing
+@testable import Fluid
+
+struct FluidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
